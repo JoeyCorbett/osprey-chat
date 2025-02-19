59e38cd88ce0065ce6e39d22364ad41e46e0e8de
Tyler Mong <117101211+tylermong@users.noreply.github.com>
2025-02-19 10:53:57 -0500

    Refactor navigation by creating NavMenu component

--- app/page.tsx
+++ app/page.tsx
@@ -1,29 +1,10 @@
-import {
-NavigationMenu,
-NavigationMenuContent,
-NavigationMenuIndicator,
-NavigationMenuItem,
-NavigationMenuLink,
-NavigationMenuList,
-NavigationMenuTrigger,
-NavigationMenuViewport,
-} from "@/components/ui/navigation-menu"
-
+import NavMenu from '../components/nav-menu'
 
 export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-      <NavigationMenu>
-        <NavigationMenuList>
-          <NavigationMenuItem>
-            <NavigationMenuTrigger>Item One</NavigationMenuTrigger>
-            <NavigationMenuContent>
-              <NavigationMenuLink>Link</NavigationMenuLink>
-            </NavigationMenuContent>
-            </NavigationMenuItem>
-          </NavigationMenuList>
-        </NavigationMenu>
+        <NavMenu />
       </main>
     </div>
   );
