66f3cc7d75b0aab25c2a6e9bea976777481e1c56
tylermong <117101211+tylermong@users.noreply.github.com>
2025-07-19 18:26:01 -0400

    feat: add component for navigation in settings page

---
+++ components/SettingsNav.tsx
@@ -0,0 +1,51 @@
+import { Palette, UserCog } from "lucide-react"
+
+import {
+  Sidebar,
+  SidebarContent,
+  SidebarGroup,
+  SidebarGroupContent,
+  SidebarGroupLabel,
+  SidebarMenu,
+  SidebarMenuButton,
+  SidebarMenuItem,
+} from "@/components/ui/sidebar"
+
+const items = [
+  {
+    title: "Profile",
+    url: "/settings/profile",
+    icon: UserCog,
+  },
+  {
+    title: "Appearance",
+    url: "/settings/appearance",
+    icon: Palette,
+  },
+]
+
+export default function SettingsNav() {
+  return (
+    <Sidebar>
+      <SidebarContent>
+        <SidebarGroup>
+          <SidebarGroupLabel>Settings</SidebarGroupLabel>
+          <SidebarGroupContent>
+            <SidebarMenu>
+              {items.map((item) => (
+                <SidebarMenuItem key={item.title}>
+                  <SidebarMenuButton asChild>
+                    <a href={item.url}>
+                      <item.icon />
+                      <span>{item.title}</span>
+                    </a>
+                  </SidebarMenuButton>
+                </SidebarMenuItem>
+              ))}
+            </SidebarMenu>
+          </SidebarGroupContent>
+        </SidebarGroup>
+      </SidebarContent>
+    </Sidebar>
+  )
+}
