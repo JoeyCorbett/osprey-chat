9262aff1208c3620748c1a9dffbaf3ee6864bd06
tylermong <117101211+tylermong@users.noreply.github.com>
2025-07-19 21:05:10 -0400

    feat: add additional content to appearance page

--- app/settings/appearance/page.tsx
+++ app/settings/appearance/page.tsx
@@ -7,12 +7,19 @@ export const metadata: Metadata = {
 
 export default async function SettingsPage() {
   return (
-    <>
-      <div className="mx-auto max-w-xl w-full px-4 mt-8">
-        <div className="w-full">
+    <div className="flex flex-col items-center justify-center w-full px-4 mt-8">
+      <div className="shadow-lg p-8 w-full max-w-lg background-card text-card-foreground border border-border rounded-[var(--radius)]">
+        <h1 className="text-xl font-bold mb-2 text-foreground">Appearance</h1>
+        <span className="text-sm text-muted-foreground">
+          Customize the look and feel of your application.
+        </span>
+        <div className="mt-6 flex items-center justify-between">
+          <span className="font-medium text-foreground space-between">
+            Theme
+          </span>
           <ThemeMenu />
         </div>
       </div>
-    </>
+    </div>
   )
 }
