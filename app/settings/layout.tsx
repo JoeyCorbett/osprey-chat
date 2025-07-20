0d4002bfd40ab4f5f9df1d7b516f1566bf5b972e
tylermong <117101211+tylermong@users.noreply.github.com>
2025-07-19 20:38:29 -0400

    fix: adjust trigger styling

--- app/settings/layout.tsx
+++ app/settings/layout.tsx
@@ -1,12 +1,12 @@
-import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar"
-import SettingsNav from "@/components/SettingsNav"
+import { SidebarProvider, SidebarTrigger } from '@/components/ui/sidebar'
+import SettingsNav from '@/components/SettingsNav'
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <SidebarProvider>
       <SettingsNav />
       <main>
-        <SidebarTrigger />
+        <SidebarTrigger className="mt-3 ml-3" />
         {children}
       </main>
     </SidebarProvider>
